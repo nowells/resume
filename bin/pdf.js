5b70625dc70c443ee46cd05985eeae376e4f850e
Nowell Strite <nowell@strite.org>
2013-08-14 21:18:02 -0400

    Updated print format to be more concise.
    Change-Id: Ic257f819e3dcf652fa89cfdbbec5582864ead127

--- bin/pdf.js
+++ bin/pdf.js
@@ -4,7 +4,18 @@ var page = require('webpage').create();
 page.open('./index.html', function () {
     page.viewportSize = { width: 921, height: 600 };
     page.paperSize = { format: 'Letter', orientation: 'portrait', margin: '1cm' };
+
     window.setTimeout(function () {
+        page.evaluate(function () {
+            var links = document.getElementsByTagName('link');
+            for (var i = 0, len = links.length; i < len; ++i) {
+                var link = links[i];
+                if (link.media == 'print') {
+                    link.media = 'all';
+                }
+            }
+        });
+
         page.render('resume.pdf');
         phantom.exit();
     }, 200);
